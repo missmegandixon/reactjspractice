94c5a7395e83978f2508ed2d0f42ac9e082d57cc
Meg Dixon <erica_dixon@apple.com>
2015-08-05 17:43:54 -0700

    working

--- drinks.js
+++ drinks.js
@@ -1,38 +1,30 @@
 /** @jsx React.DOM */
 
 var drinks = [
-	{vodka: ['screwdriver', 'majito']},
-	{gin: ['corpse reviver #2', 'gin & tonic']},
-	{rum: ['rum & coke', 'rum & coke']},
-	{tequilla: ['margarita', 'straight']}
+	{liquor: 'vodka', drinks: ['screwdriver', 'mojito']},
+	{liquor: 'gin', drinks: ['corpse reviver #2', 'gin & tonic']},
+	{liquor: 'rum', drinks: ['rum & coke', 'rum & coke']},
+	{liquor: 'tequila', drinks: ['margarita', 'straight']}
 ];
-
-var LiquorList = React.createClass({
-  render: function() {
-    return <li>{this.props.drinkData}</li>;
-  }
-});
 
 var DrinkList = React.createClass({
   render: function() {
-  var listData = this.props.data.map(function(drink) {
-    return <LiquorList drinkData={drink}></LiquorList>;
-  });
+  	var listData = this.props.data.map(function(item) {
+    	return <LiquorList liquorName={item.liquor} drinkNames={item.drinks}></LiquorList>;
+  	});
 	  return <ul>{listData}</ul>;
   }
 });
 
-var Drink = React.createClass({
+var LiquorList = React.createClass({
   render: function() {
-    return (
-			<ul>
-				{drinks.map(function(drink){
-					return <li key={drink.id}>{drink.text} </li>;
-		})}
-	</ul>
-);
-}
+		var listItems = this.props.drinkNames.map(function(name) {
+			return (<li>{name}</li>);
+		});
+    return <span><h3>{this.props.liquorName}</h3><ul>{listItems}</ul></span>;
+  }
+});
 
 React.render(
-  <listData data={drinks} />, document.body
+  <DrinkList data={drinks} />, document.body
 );
